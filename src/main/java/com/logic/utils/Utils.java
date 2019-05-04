3864bf0a7b90c53ec8b5e6a9584173147b6122f0
martinV97 <martin.vn11@gmail.com>
2019-05-04 16:04:22 -0500

    Poker Completo

--- src/main/java/com/logic/utils/Utils.java
+++ src/main/java/com/logic/utils/Utils.java
@@ -35,12 +35,6 @@ public class Utils {
         return Integer.parseInt(iterate)>0;
     }
 
-    public static List<String> convertDoubleToString(List<Double> nums) {
-        List<String> strings = new ArrayList<>();
-        nums.forEach(e -> strings.add(e.toString()));
-        return strings;
-    }
-
     public static double[] convertDoubleToDoubleArray(List<Double> nums) {
         double[] ds = new double[nums.size()];
         for (int i = 0; i < nums.size(); i++) ds[i] = ds[i] = nums.get(i);
